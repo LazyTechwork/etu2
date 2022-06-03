09a67c0988ddc9aa62884960df70c075edd1ed84
Ivan Petrov <lazytechwork@gmail.com>
2022-06-03 21:38:16 +0300

    Final

--- NSFileManager.cpp
+++ NSFileManager.cpp
@@ -21,98 +21,94 @@ int main(int argc, char *argv[]) {
     const std::string lib_wave = current_directory + "lib_wave.exe";
     const std::string lib_bmpcode = current_directory + "lib_bmpcode.exe";
     const std::string lib_bytelysis = current_directory + "lib_bytelysis.exe";
-    int key, selectedFile;
+    int key = -1, selectedFile;
     COORD selectedFileConsolePos;
     CONSOLE_SCREEN_BUFFER_INFO csbiInfo;
     HANDLE stdHandle = GetStdHandle(STD_OUTPUT_HANDLE);
     std::vector<WIN32_FIND_DATA> directoryListing;
     bool isDirectoryDirty = true;
 
-
-//    while(true){
-//        int k = _getch();
-//        std::cout << k << std::endl;
-//        if (k == 'Q')
-//            return 0;
-//    }
-
-
     do {
-        if (key == ARROW_PREFIX) {
-            switch ((key = _getch())) {
-                case ARROW_DOWN:
-                    ++selectedFile;
-                    if (selectedFile >= directoryListing.size())
-                        selectedFile = 0;
-                    break;
-                case ARROW_UP:
-                    --selectedFile;
-                    if (selectedFile < 0)
-                        selectedFile = directoryListing.size() - 1;
-                    break;
-            }
-        } else if (key == FUNC_PREFIX) {
-            switch ((key = _getch())) {
-                case FUNC_1:
-                    if (!FilesystemReader::isDirectory(directoryListing[selectedFile])) {
-                        std::string command = R"(start "" ")";
-                        command
-                                .append(lib_wave)
-                                .append("\" \"")
-                                .append(current_directory)
-                                .append(directoryListing[selectedFile].cFileName)
-                                .append("\"");
-                        system(command.c_str());
-                    }
-                    break;
-                case FUNC_2:
-                    if (!FilesystemReader::isDirectory(directoryListing[selectedFile])) {
-                        std::string command = R"(start "" ")";
-                        command
-                                .append(lib_bmpcode)
-                                .append("\" -encode \"")
-                                .append(current_directory)
-                                .append(directoryListing[selectedFile].cFileName)
-                                .append("\"");
-                        system(command.c_str());
-                    }
-                    break;
-                case FUNC_3:
-                    system("cls");
-                    std::cout << std::endl << "Make byte analysis" << std::endl << "Enter lower value (float 0-1): ";
-                    float arg;
-                    std::cin >> arg;
+        if (key != -1)
+            if (key == ARROW_PREFIX) {
+                switch ((key = _getch())) {
+                    case ARROW_DOWN:
+                        ++selectedFile;
+                        if (selectedFile >= directoryListing.size())
+                            selectedFile = 0;
+                        break;
+                    case ARROW_UP:
+                        --selectedFile;
+                        if (selectedFile < 0)
+                            selectedFile = directoryListing.size() - 1;
+                        break;
+                }
+            } else if (key == FUNC_PREFIX) {
+                switch ((key = _getch())) {
+                    case FUNC_1:
+                        if (!FilesystemReader::isDirectory(directoryListing[selectedFile])) {
+                            std::string command = R"(start "" ")";
+                            command
+                                    .append(lib_wave)
+                                    .append("\" \"")
+                                    .append(current_directory)
+                                    .append(directoryListing[selectedFile].cFileName)
+                                    .append("\"");
+                            system(command.c_str());
+                        }
+                        break;
+                    case FUNC_2:
+                        if (!FilesystemReader::isDirectory(directoryListing[selectedFile])) {
+                            std::string command = R"(start "" ")";
+                            command
+                                    .append(lib_bmpcode)
+                                    .append("\" -encode \"")
+                                    .append(current_directory)
+                                    .append(directoryListing[selectedFile].cFileName)
+                                    .append("\"");
+                            system(command.c_str());
+                        }
+                        break;
+                    case FUNC_3:
+                        if (!FilesystemReader::isDirectory(directoryListing[selectedFile])) {
+                            system("cls");
+                            std::cout << std::endl << "Make byte analysis" << std::endl
+                                      << "Enter lower value (float 0-1): ";
+                            float arg;
+                            std::cin >> arg;
+                            std::string command = R"(start "" ")";
+                            command
+                                    .append(lib_bytelysis)
+                                    .append("\" \"")
+                                    .append(current_directory)
+                                    .append(directoryListing[selectedFile].cFileName)
+                                    .append("\" --tr ")
+                                    .append(std::to_string(arg));
+                            system(command.c_str());
+                            std::cout << std::endl;
+                        }
+                        break;
+                }
+            } else if (key == KEY_RETURN) {
+                if (FilesystemReader::isDirectory(directoryListing[selectedFile])) {
+                    SetCurrentDirectory(
+                            std::string(current_directory + directoryListing[selectedFile].cFileName).c_str());
+                    GetCurrentDirectory(MAX_PATH, curDir);
+                    current_directory = std::string(curDir) + std::string("\\");
+                    isDirectoryDirty = true;
+                } else {
                     std::string command = R"(start "" ")";
                     command
-                            .append(lib_bytelysis)
-                            .append("\" \"")
                             .append(current_directory)
                             .append(directoryListing[selectedFile].cFileName)
-                            .append("\" --tr ")
-                            .append(std::to_string(arg));
+                            .append("\"");
                     system(command.c_str());
-                    std::cout << std::endl;
-                    break;
-            }
-        } else if (key == KEY_RETURN) {
-            if (FilesystemReader::isDirectory(directoryListing[selectedFile])) {
-                SetCurrentDirectory(std::string(current_directory + directoryListing[selectedFile].cFileName).c_str());
-                GetCurrentDirectory(MAX_PATH, curDir);
-                current_directory = std::string(curDir) + std::string("\\");
+                }
+            } else if (key == CTRL_R) {
                 isDirectoryDirty = true;
-            } else {
-                std::string command = R"(start "" ")";
-                command
-                        .append(current_directory)
-                        .append(directoryListing[selectedFile].cFileName)
-                        .append("\"");
-                system(command.c_str());
+            } else if (key == CTRL_C) {
+                return 0;
             }
-        } else if (key == CTRL_R) {
-            isDirectoryDirty = true;
-        } else if (key == CTRL_C) {
-            return 0;
-        }
         GetConsoleScreenBufferInfo(stdHandle, &csbiInfo);
         system("cls");
 
@@ -151,7 +147,7 @@ int main(int argc, char *argv[]) {
         SetConsoleTextAttribute(stdHandle, HIGHLIGHTED_TEXT);
         std::cout << " ^C ";
         SetConsoleTextAttribute(stdHandle, STANDART_TEXT);
-        std::cout << " To quit ";
+        std::cout << " Quit ";
         std::cout << std::endl << std::endl << "Current directory: " << current_directory << std::endl << std::endl;
 
         for (int i = 0, entriesTotal = directoryListing.size(); i < entriesTotal; ++i) {
